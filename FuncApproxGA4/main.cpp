1062c59a87a0de17f9319fd310fa0c5fef688ce3
Brandon Brown <brandonb@Brandon-Browns-MacBook-Pro.local>
2011-07-29 12:13:01 -0700

    Initial Commit

---
+++ FuncApproxGA4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FuncApproxGA4
+//
+//  Created by Brandon Brown on 7/29/11.
+//  Copyright 2011 UCLA. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
